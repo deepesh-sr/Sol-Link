9d6287c530c44426ead72ef2fb99394adf4c69fd
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-03-17 11:09:42 +0530

    chore: modularised the routes and main.rs files

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,28 +1,10 @@
-use axum::{Json, Router, extract::State, http::StatusCode, routing::post};
-use serde::Deserialize;
+mod routes;
+ 
 use std::error::Error;
-use std::net::SocketAddr;
+use axum::{Router, routing::post};
 use store::Store;
-use bcrypt::{hash,DEFAULT_COST};
 
-#[derive(Deserialize)]
-struct SignupRequest {
-    email: String,
-    password: String,
-    public_key: Option<String>,
-}
-
-async fn signup_handler(
-    State(store): State<Store>,
-    Json(payload): Json<SignupRequest>,
-) -> Result<Json<store::user::User>, (axum::http::StatusCode, String)> {
-
-    let hashed_password = hash(payload.password , DEFAULT_COST).map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Failed to hash password".to_string()))?;
-
-    let user = store.create_user(&payload.email, &hashed_password, payload.public_key.as_deref()).await.map_err(|e| (axum::http::StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
-
-    Ok(Json(user))
-}
+use routes::user::signup_handler;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
--- backend/src/routes/user.rs
+++ backend/src/routes/user.rs
@@ -1,1 +1,23 @@
+use axum::{Json, extract::State, http::StatusCode};
+use serde::Deserialize;
+use store::Store;
+use bcrypt::{hash,DEFAULT_COST};
 
+#[derive(Deserialize)]
+pub struct SignupRequest {
+    email: String,
+    password: String,
+    public_key: Option<String>,
+}
+
+pub async fn signup_handler(
+    State(store): State<Store>,
+    Json(payload): Json<SignupRequest>,
+) -> Result<Json<store::user::User>, (axum::http::StatusCode, String)> {
+
+    let hashed_password = hash(payload.password , DEFAULT_COST).map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Failed to hash password".to_string()))?;
+
+    let user = store.create_user(&payload.email, &hashed_password, payload.public_key.as_deref()).await.map_err(|e| (axum::http::StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    Ok(Json(user))
+}
