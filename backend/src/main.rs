64af5edf9f17f81bdbaa47b70061a37ba1060312
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-03-19 11:48:40 +0530

    feat : add jupiter quote

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,7 +5,7 @@ use axum::{Router, routing::{get, post}};
 use store::Store;
 
 use routes::user::{signup_handler, signin_handler , get_user};
-use crate::routes::solana::get_sol_balance;
+use crate::routes::solana::{get_quote, get_sol_balance};
 
 
 
@@ -21,7 +21,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .route("/api/v1/signup", post(signup_handler))
         .route("/api/v1/signin", post(signin_handler))
         .route("/api/v1/user", get(get_user))
-        .route("/api/sol", get(get_sol_balance))
+        .route("/api/v1/sol", get(get_sol_balance))
+        .route("/api/v1/quote", get(get_quote))
         .with_state(store);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4444").await.unwrap();
--- backend/src/routes/solana.rs
+++ backend/src/routes/solana.rs
@@ -1,14 +1,20 @@
 use std::str::FromStr;
 
-use axum::{Json, extract::State, http::StatusCode};
-use serde::Serialize;
+use axum::{Json, extract::{Query, State}, http::StatusCode};
+use serde::{Deserialize, Serialize};
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::{lamports, pubkey::{self, Pubkey}};
 use store::Store;
 
 use crate::routes::Claims;
 
-
+#[derive(Deserialize)]
+pub struct QuoteRequest{
+    pub input_mint : String, 
+    pub output_mint : String, 
+    pub amount : u64, 
+    pub slippage_bps : Option<u16>
+}
 
 #[derive(Serialize)]
 pub struct BalanceResponse{
@@ -39,4 +45,30 @@ pub async fn get_sol_balance(
     let sol_balance = lamports as f64 / 1_000_000_000.0;
 
     Ok(Json(BalanceResponse { sol_balance }))
+}
+
+pub async fn get_quote(Query(params) : Query<QuoteRequest>)-> Result<Json<serde_json::Value>, (StatusCode , String)> {
+
+    let api_key = std::env::var("JUPITER_API_KEY")
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "JUPITER_API_KEY not set".to_string()))?;
+
+    // Jupiter Quote API URL
+    let url = format!(
+        "https://api.jup.ag/swap/v1/quote?inputMint={}&outputMint={}&amount={}&slippageBps={}",
+        params.input_mint,
+        params.output_mint,
+        params.amount,
+        params.slippage_bps.unwrap_or(50) // Default 0.5% slippage
+    );
+
+    let client = reqwest::Client::new();
+    let response = client.get(&url)
+        .header("x-api-key", &api_key)
+        .send()
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
+        .json::<serde_json::Value>()
+        .await.map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    Ok(Json(response))
 }
