b6cefcc4378b333bc61be2ea95ae3b318b1e0af2
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-03-25 23:42:05 +0530

    wip: dkgg_route2_handler added

--- mpc/src/main.rs
+++ mpc/src/main.rs
@@ -1,22 +1,59 @@
-use std::{collections::HashMap, sync::Mutex};
+use std::{collections::HashMap, env, sync::Arc};
+use tokio::sync::Mutex;
 
+use axum::{Router, routing::post};
 use frost_ed25519::{Identifier, keys::dkg, round1::SigningNonces};
-use sqlx::PgPool;
+
+use dotenv;
+use sqlx::{PgPool, postgres::PgPoolOptions};
+use tokio;
 use uuid::Uuid;
 
-pub mod tss;
 pub mod routes;
+pub mod tss;
 
-
-struct Appstate{
-
-    node_id : Identifier,
-    dkg_round1_secrets : Mutex<HashMap<Uuid , dkg::round1::SecretPackage>>,
-    dkg_round2_secrets : Mutex<HashMap<Uuid , dkg::round2::SecretPackage>>,
-    sign_nonces : Mutex<HashMap<Uuid, SigningNonces>>,
-    db : PgPool
+struct Appstate {
+    node_id: Identifier,
+    dkg_round1_secrets: Mutex<HashMap<Uuid, dkg::round1::SecretPackage>>,
+    dkg_round2_secrets: Mutex<HashMap<Uuid, dkg::round2::SecretPackage>>,
+    sign_nonces: Mutex<HashMap<Uuid, SigningNonces>>,
+    db: PgPool,
 }
-fn main() {
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
 
+    let node_id: u16 = env::var("NODE_ID").unwrap().parse().unwrap();
+    let identifier = Identifier::try_from(node_id).unwrap();
+    let db_url = env::var("DATABASE_URL")?;
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(db_url.as_str())
+        .await?;
+    let port = env::var("PORT").unwrap_or("5001".to_string());
+
+    let state = Arc::new(Appstate {
+        node_id: identifier,
+        dkg_round1_secrets: Mutex::new(HashMap::new()),
+        dkg_round2_secrets: Mutex::new(HashMap::new()),
+        sign_nonces: Mutex::new(HashMap::new()),
+        db: pool,
+    });
+
+    let app = Router::new()
+        .route("/dkg/round1", post(dkg_route1_handler))
+        .route("/dkg/round2", post(dkg_route2_handler))
+        .route("/dkg/round3", post(dkg_round3_handler))
+        .route("/sign/round1", post(sign_round1_handler))
+        .route("/sign/round2", post(sign_round2_handler))
+        .with_state(state);
+
+    let address = format!("0.0.0.0:{}",port);
+    let listener = tokio::net::TcpListener::bind(&address)
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
--- mpc/src/routes/dkg.rs
+++ mpc/src/routes/dkg.rs
@@ -1,6 +1,7 @@
-use std::sync::Arc;
+use std::{collections::BTreeMap, sync::Arc};
 
 use axum::{Json, extract::State};
+use frost_ed25519::{Identifier, keys::dkg::round1};
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -34,5 +35,30 @@ pub async fn dkg_route1_handler(
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
     Ok(Json(DkgRound1Response {
         round1_package: package_json,
+    }))
+}
+
+#[derive(Deserialize)]
+pub struct DkgRound2Request{
+    pub user_id : Uuid, 
+    pub round1_packages : BTreeMap<Identifier, round1::Package>
+}
+
+#[derive(Serialize)]
+pub struct DkgRound2Response{
+    pub round2_package : serde_json::Value
+}
+pub async fn dkg_route2_handler(
+    State(state) : State<Arc<Appstate>>,
+    Json(payload) : Json<DkgRound2Request>
+)-> Result<Json<DkgRound2Response>, ( StatusCode, String)>{
+
+    let secret = state.dkg_round1_secrets.lock().await.remove(&payload.user_id).ok_or((StatusCode::BAD_REQUEST, "No round1 secret found for round 1".to_string()))?;
+
+    let (round2_secret, round2_package) = tss::dkg_part2(secret, &payload.round1_packages).map_err(|e|(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    let round2_package_json = serde_json::to_value(&round2_package).map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    Ok(Json(DkgRound2Response {
+        round2_package: round2_package_json,
     }))
 }
