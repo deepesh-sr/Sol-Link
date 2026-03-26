ce47ef45ebeaff7a00696996404466ea5e7989a6
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-03-27 01:16:54 +0530

    feat: mpc implementation added

--- mpc/src/main.rs
+++ mpc/src/main.rs
@@ -13,6 +13,7 @@ pub mod routes;
 pub mod tss;
 
 use routes::dkg::{dkg_route1_handler, dkg_route2_handler, dkg_round3_handler};
+use routes::sign::{sign_round1_handler,sign_round2_handler};
 
 pub struct Appstate {
     node_id: Identifier,
@@ -47,9 +48,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/dkg/round1", post(dkg_route1_handler))
         .route("/dkg/round2", post(dkg_route2_handler))
         .route("/dkg/round3", post(dkg_round3_handler))
-
-        // .route("/sign/round1", post(sign_round1_handler))
-        // .route("/sign/round2", post(sign_round2_handler))
+        .route("/sign/round1", post(sign_round1_handler))
+        .route("/sign/round2", post(sign_round2_handler))
         .with_state(state);
 
     let address = format!("0.0.0.0:{}",port);
--- mpc/src/routes/sign.rs
+++ mpc/src/routes/sign.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use axum::{Json, extract::State};
-use frost_ed25519::{keys::KeyPackage, round1::SigningCommitments};
+use frost_ed25519::{SigningPackage, keys::KeyPackage, round1::SigningCommitments, round2::SignatureShare};
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -41,4 +41,37 @@ pub async fn sign_round1_handler(
 
 
 Ok(Json(SignRound1Response{commitments : commitments}))
+}
+
+#[derive(Deserialize)]
+pub struct SignRound2Request{
+    pub user_id : Uuid,
+    pub signing_package : SigningPackage
+}
+
+#[derive(Serialize)]
+pub struct SignRound2Response{
+    signature_share : SignatureShare
+}
+pub async fn sign_round2_handler(
+    State(state) : State<Arc<Appstate>>,
+    Json(payload) : Json<SignRound2Request>
+)-> Result<Json<SignRound2Response>,(StatusCode,String)>{
+
+    let sign_nonces = state.sign_nonces.lock().await.remove(&payload.user_id).ok_or((StatusCode::BAD_REQUEST, "No round1 secret found for round 1".to_string()))?;
+    let key_package_bytes : Vec<u8>  = sqlx::query_scalar(
+        r#"
+            SELECT key_package FROM keyshares WHERE user_id = $1    
+        "#
+    ).bind(payload.user_id)
+    .fetch_one(&state.db)
+    .await
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+
+    let key_package : KeyPackage = serde_json::from_slice(&key_package_bytes).map_err(|e|(StatusCode::INTERNAL_SERVER_ERROR , e.to_string()))?;
+
+   let signature_share=  tss::sign_round2(payload.signing_package, &sign_nonces, &key_package).map_err(|e|(StatusCode::INTERNAL_SERVER_ERROR , e.to_string()))?;
+
+    Ok(Json(SignRound2Response{signature_share : signature_share}))
 }
